2ee77afefc829000d13d33f32b485c331bddbf6b
AliKose50 <darkklose_ali@hotmail.com>
2025-08-05 17:21:43 +0300

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebSıtesı.Models;
+
+namespace WebSıtesı.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
